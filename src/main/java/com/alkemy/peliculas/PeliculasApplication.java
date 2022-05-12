1545de484a2b9aaa3c43714e097118c3a25f34fb
Camilo Abarca <cabarcaz@yahoo.cl>
2022-05-12 12:42:32 -0400

    primer commit

---
+++ src/main/java/com/alkemy/peliculas/PeliculasApplication.java
@@ -0,0 +1,14 @@
+package com.alkemy.peliculas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeliculasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeliculasApplication.class, args);
+	}
+
+}
+
